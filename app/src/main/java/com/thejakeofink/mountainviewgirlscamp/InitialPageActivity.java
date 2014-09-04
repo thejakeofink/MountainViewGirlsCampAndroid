0f86617df72b5de487785f913644c1cc8cf617f3
Jake Stokes <jstokes@sorenson.com>
2014-09-03 21:50:46 -0600

    Added share icon for action bar

--- app/src/main/java/com/thejakeofink/mountainviewgirlscamp/InitialPageActivity.java
+++ app/src/main/java/com/thejakeofink/mountainviewgirlscamp/InitialPageActivity.java
@@ -1,36 +1,38 @@
 package com.thejakeofink.mountainviewgirlscamp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
-public class InitialPageActivity extends Activity {
+public class InitialPageActivity extends Activity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_initial_page);
-    }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.initial_page, menu);
-        return true;
+        ((Button)findViewById(R.id.btn_pictures)).setOnClickListener(this);
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_study_guides:
+                // TODO: go to study guides...
+                break;
+            case R.id.btn_pictures:
+                // TODO: go to pictures...
+                Intent albumsIntent = new Intent(this, FlickrPhotoAlbumActivity.class);
+                startActivity(albumsIntent);
+                break;
+            case R.id.btn_quotes:
+                // TODO: go to quotes...
+                break;
         }
-        return super.onOptionsItemSelected(item);
     }
 }
